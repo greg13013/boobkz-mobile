53a8a7a36d6ad0f3612df2517efb2b95e92470cd
Colli Gregory <colli.gregory.13@gmail.com>
2021-10-27 21:08:04 +0200

    modif mobile style

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    MDBBootstrapModule.forRoot(),
     HomePageRoutingModule,
     ReactiveFormsModule,
   ],
